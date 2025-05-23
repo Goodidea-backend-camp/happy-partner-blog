0a5468e858d4cbe13c48dde75bf26d0ec80262a7
JYu1999 <jk29666338@gmail.com>
2025-05-23 21:41:24 +0800

    fix: 註解掉不需要的 route

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,18 @@
 
 use App\Http\Controllers\Frontend\PostController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 // Blog routes
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+/**
+ * TODO: 目前暫時不開放前台使用者進入後台，但之後預期會開放，因此使用註解。
+ * 後續開放需驗證 Route 有效性。
+ */
+//Route::get('dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//require __DIR__.'/settings.php';
+//require __DIR__.'/auth.php';
