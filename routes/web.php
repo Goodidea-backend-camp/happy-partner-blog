af2fe319280b5f9b8ad661c6c37155a014546d10
CHIEH-YU CHAN <67377638+JYu1999@users.noreply.github.com>
2025-05-23 22:30:51 +0800

    [HPB-16] [Bug] 不應該有 /dashboard 和 /login 的 route (#20)

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,17 @@
 
 use App\Http\Controllers\Frontend\PostController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 // Blog routes
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+/**
+ * TODO: 目前不開放前台使用者進入後台，但尚無時間檢查並刪除相關檔案，因此暫時用註解讓使用者無法訪問，後續應考慮將相關檔案刪除
+ */
+//Route::get('dashboard', function () {
+//    return Inertia::render('Dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+//
+//require __DIR__.'/settings.php';
+//require __DIR__.'/auth.php';
