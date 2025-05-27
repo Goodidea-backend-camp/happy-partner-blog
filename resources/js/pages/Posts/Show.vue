22d5a9cc4e79c8fab62534484ed4c0c15fa2c84b
JYu1999 <jk29666338@gmail.com>
2025-05-27 23:27:39 +0800

    fix: 前端 bug

--- resources/js/pages/Posts/Show.vue
+++ resources/js/pages/Posts/Show.vue
@@ -150,7 +150,7 @@ function formatDate(dateString: string) {
   margin: 1em 0;
   white-space: pre-wrap;       /* Since CSS 2.1 */
   white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
-  white-space: -pre-wrap;      /* Opera 4-6 */
+  white-space: pre-wrap;      /* Opera 4-6 */
   white-space: -o-pre-wrap;    /* Opera 7 */
   word-wrap: break-word;       /* Internet Explorer 5.5+ */
 }
