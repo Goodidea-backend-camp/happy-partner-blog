be72edb009922922015048c06149dba33c627f86
CHIEH-YU CHAN <67377638+JYu1999@users.noreply.github.com>
2025-07-16 22:31:06 +0800

    [HPB-86] [FE] 選擇 PrimeVue 整合策略，並配置到專案中 (#34)

--- resources/js/composables/usePrimeVue.ts
+++ resources/js/composables/usePrimeVue.ts
@@ -3,7 +3,12 @@ import PrimeVue from "primevue/config";
 
 export function initializePrimeVue(app: App) {
     app.use(PrimeVue, {
-        // TODO: 目前尚未確認是否使用 unstyled 模式，如果後續確認使用 unstyled 模式，應考慮將 `@primeuix/themes` 套件刪除
-        unstyled: true
+        unstyled: true,
+        pt: {
+            /**
+             * Defines the shared pass through properties per component type.
+             * @see https://primevue.org/passthrough
+             */
+        }
     });
 }
