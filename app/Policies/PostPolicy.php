5a98af0ff225bb786aac81b8cabc5cd92e341a8b
Way <k8652090202@gmail.com>
2025-07-20 02:59:15 +0800

    style:註解調整成PHPDoc格式

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -7,57 +7,114 @@ use App\Models\User;
 
 class PostPolicy
 {
-    // 判斷是否允許登入使用者更新特定文章
+    /**
+     * 判斷是否允許登入使用者更新特定文章
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function update(User $user, Post $post): bool
     {
-        return $user->id === $post->user_id || $user->role === 'admin';
+        // 管理員或作者可更新
+        return $user->role === 'admin' || $user->id === $post->user_id;
     }
 
-    // 判斷是否允許登入使用者刪除特定文章
+    /**
+     * 判斷是否允許登入使用者刪除特定文章
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function delete(User $user, Post $post): bool
     {
         return $user->id === $post->user_id;
     }
 
-    // 判斷是否允許登入使用者還原特定文章
+    /**
+     * 判斷是否允許登入使用者還原特定文章
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function restore(User $user, Post $post): bool
     {
         return $user->role === 'admin';
     }
 
-    // 判斷是否允許登入使用者永久刪除特定文章
+    /**
+     * 判斷是否允許登入使用者永久刪除特定文章
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function forceDelete(User $user, Post $post): bool
     {
         return false;
     }
 
-    // 判斷是否允許登入使用者編輯文章標題
+    /**
+     * 判斷是否允許登入使用者編輯文章標題
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function editTitle(User $user, Post $post): bool
     {
         return $user->id === $post->user_id;
     }
 
-    // 判斷是否允許登入使用者編輯文章 slug
+    /**
+     * 判斷是否允許登入使用者編輯文章 slug
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function editSlug(User $user, Post $post): bool
     {
         return $user->id === $post->user_id;
     }
 
-    // 判斷是否允許登入使用者編輯文章內容
+    /**
+     * 判斷是否允許登入使用者編輯文章內容
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function editContent(User $user, Post $post): bool
     {
         return $user->id === $post->user_id;
     }
 
-    // 判斷是否允許登入使用者編輯文章狀態
+    /**
+     * 判斷是否允許登入使用者編輯文章狀態
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function editStatus(User $user, Post $post): bool
     {
-        return $user->role === 'admin' or $user->id === $post->user_id;
+        // 管理員或作者可編輯文章狀態
+        return $user->role === 'admin' || $user->id === $post->user_id;
     }
 
-    // 判斷是否顯示刪除按鈕
+    /**
+     * 判斷是否顯示刪除按鈕
+     *
+     * @param User $user
+     * @param Post $post
+     * @return bool
+     */
     public function viewDeleteButton(User $user, Post $post): bool
     {
-        return $user->id === $post->user_id || $user->role === 'admin';
+        // 管理員或作者可見刪除按鈕
+        return $user->role === 'admin' || $user->id === $post->user_id;
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -6,85 +6,157 @@ use App\Models\User;
 
 class UserPolicy
 {
-    // 判斷使用者是否可以更新特定使用者。
+    /**
+     * 判斷使用者是否可以更新特定使用者
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function update(User $user, User $targetUser): bool
     {
-        return $user->id === $targetUser->id || $user->role === 'admin';
+        // 管理員或本人可更新
+        return $user->role === 'admin' || $user->id === $targetUser->id;
     }
 
-    // 判斷使用者是否可以刪除特定使用者。
+    /**
+     * 判斷使用者是否可以刪除特定使用者
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function delete(User $user, User $targetUser): bool
     {
         return false;
     }
 
-    // 判斷使用者是否可以還原特定使用者。
+    /**
+     * 判斷使用者是否可以還原特定使用者
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function restore(User $user, User $targetUser): bool
     {
         return $user->role === 'admin';
     }
 
-    // 判斷使用者是否可以永久刪除特定使用者。
+    /**
+     * 判斷使用者是否可以永久刪除特定使用者
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function forceDelete(User $user, User $targetUser): bool
     {
         return false;
     }
 
-    // 判斷使用者是否可以編輯名稱欄位。
+    /**
+     * 判斷使用者是否可以編輯名稱欄位
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function editName(User $user, User $targetUser): bool
     {
+        // 當 $targetUser id 為 null，代表此時正在新增使用者
         if ($targetUser->id === null) {
+            // admin 才可新增使用者
             return $user->role === 'admin';
         }
-
+        // 僅本人可編輯姓名欄位
         return $user->id === $targetUser->id;
     }
 
-    // 判斷使用者是否可以編輯信箱欄位。
+    /**
+     * 判斷使用者是否可以編輯信箱欄位
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function editEmail(User $user, User $targetUser): bool
     {
         if ($targetUser->id === null) {
             return $user->role === 'admin';
         }
-
+        // 僅本人可編輯mail欄位
         return $user->id === $targetUser->id;
     }
 
-    // 判斷使用者是否可以編輯密碼欄位。
+    /**
+     * 判斷使用者是否可以編輯密碼欄位
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function editPassword(User $user, User $targetUser): bool
     {
         if ($targetUser->id === null) {
             return $user->role === 'admin';
         }
-
+        // 僅本人可編輯密碼欄位
         return $user->id === $targetUser->id;
     }
 
-    // 判斷使用者是否可以編輯密碼確認欄位。
+    /**
+     * 判斷使用者是否可以編輯密碼確認欄位
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function editPasswordConfirmation(User $user, User $targetUser): bool
     {
         if ($targetUser->id === null) {
             return $user !== null;
         }
-
+        // 僅本人可編輯密碼確認欄位
         return $user->id === $targetUser->id;
     }
 
-    // 判斷使用者是否可以編輯角色欄位。
+    /**
+     * 判斷使用者是否可以編輯角色欄位
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function editRole(User $user, User $targetUser): bool
     {
+        // 僅管理員可編輯角色
         return $user->role === 'admin';
     }
 
-    // 判斷使用者是否可以檢視角色欄位。
+    /**
+     * 判斷使用者是否可以檢視角色欄位
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function viewRole(User $user, User $targetUser): bool
     {
+        // 僅管理員可檢視角色
         return $user->role === 'admin';
     }
 
-    // 判斷是否顯示編輯按鈕。
+    /**
+     * 判斷是否顯示編輯按鈕
+     *
+     * @param User $user
+     * @param User $targetUser
+     * @return bool
+     */
     public function viewEditButton(User $user, User $targetUser): bool
     {
+        // 管理員或本人可見
         return $user->role === 'admin' || $user->id === $targetUser->id;
     }
 }
