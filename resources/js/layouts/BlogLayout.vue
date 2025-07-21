d2ae9ad423759a97bea08d9321877c4ee3f0f455
CHIEH-YU CHAN <67377638+JYu1999@users.noreply.github.com>
2025-07-21 10:29:37 +0800

    [HPB-87] [FE] 新增 BlogFooter 組件並移除 BlogLayout 中的 footer 內容 (#39)

--- resources/js/layouts/BlogLayout.vue
+++ resources/js/layouts/BlogLayout.vue
@@ -19,17 +19,13 @@
       </div>
     </main>
 
-    <footer class="bg-gray-800 text-white mt-auto py-6">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-sm">
-        &copy; {{ new Date().getFullYear() }} 快樂小夥伴的部落格. All rights reserved.
-        <p class="text-xs text-gray-400 mt-1">Powered by Laravel & Vue</p>
-      </div>
-    </footer>
+    <BlogFooter />
   </div>
 </template>
 
 <script setup lang="ts">
 import { Link, Head } from '@inertiajs/vue3';
+import BlogFooter from '@/components/BlogFooter.vue';
 </script>
 
 <style scoped>
