161eeded754c3b16aeaf3a5b9a18a6b1e674a5e8
chungkai1029 <chungkai1029@gmail.com>
2025-07-23 23:21:53 +0800

    fix: Fix Navbar out of range issue

--- resources/js/components/BlogHeader.vue
+++ resources/js/components/BlogHeader.vue
@@ -2,7 +2,7 @@
     <header class="bg-white dark:bg-gray-800 shadow-sm sticky top-0 z-50">
         <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center">
-                <h1 class="text-2xl font-semibold text-gray-900 dark:text-white">
+                <h1 class="text-xl sm:text-2xl font-semibold text-gray-900 dark:text-white truncate">
                     <Link :href="route('posts.index')">快樂小夥伴的部落格</Link>
                 </h1>
             </div>
