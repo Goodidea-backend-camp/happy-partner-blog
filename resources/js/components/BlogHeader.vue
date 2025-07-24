dc27220e28d8f5c1a3294a7b3125900710c0a8cb
Chang, Chung-Kai <57675957+chungkai1029@users.noreply.github.com>
2025-07-24 09:20:08 +0800

    [HPB-74] [FE] 將 BlogLayout 的 Navbar 元件化 (#41)

---
+++ resources/js/components/BlogHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="bg-white dark:bg-gray-800 shadow-sm sticky top-0 z-50">
+        <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between items-center">
+                <h1 class="text-xl sm:text-2xl font-semibold text-gray-900 dark:text-white truncate">
+                    <Link :href="route('posts.index')">快樂小夥伴的部落格</Link>
+                </h1>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+</script>
