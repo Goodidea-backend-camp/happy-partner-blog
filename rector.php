da9976cef44a22ae84a7ef91f19ad589ac818e0b
linhn0617 <yyo833782@gmail.com>
2025-07-30 08:06:37 +0800

    style: 修正 pint 出現的錯誤

--- rector.php
+++ rector.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
-use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
-use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\Config\RectorConfig;
 use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
@@ -14,7 +10,6 @@ use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationRector;
 use Rector\TypeDeclaration\Rector\Closure\AddClosureVoidReturnTypeWhereNoReturnRector;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
@@ -23,7 +18,7 @@ return RectorConfig::configure()
         __DIR__.'/bootstrap',
         __DIR__.'/routes',
         __DIR__.'/tests',
-        __DIR__.'/config'
+        __DIR__.'/config',
     ])
     ->withRules([
         AddReturnTypeDeclarationRector::class,
